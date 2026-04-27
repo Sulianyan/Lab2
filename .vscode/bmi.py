9b397f07e1bf8ad56815b49b796a72fab343dad6
Koishi <hongkai.25@ichat.sp.edu.sg>
2026-04-27 13:50:25 +0800

    67543

---
+++ .vscode/bmi.py
@@ -0,0 +1,6 @@
+def calculate_bmi(height,weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+    bmi = weight/(height*height)
+    print("bmi is " + str(bmi))
+calculate_bmi(weight = 60, height = 1.75)

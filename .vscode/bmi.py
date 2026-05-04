f09d29080e56dace3e611580ec8e754420c34c99
Koishi <hongkai.25@ichat.sp.edu.sg>
2026-05-04 13:44:02 +0800

    sssssss

--- .vscode/bmi.py
+++ .vscode/bmi.py
@@ -3,4 +3,15 @@ def calculate_bmi(height,weight):
     print("Weight = " + str(weight))
     bmi = weight/(height*height)
     print("bmi is " + str(bmi))
+    if (bmi < 18.5):
+        print("Underweight")
+        return -1
+    elif (bmi >= 18.5 and bmi < 25):
+        print("Normal weight")
+        return 0
+    elif (bmi >= 25 and bmi < 30):
+        print("Overweight")
+        return 1
+    else:
+        print("Obese")
 calculate_bmi(weight = 60, height = 1.75)
